b73eeb70efc0230d8a95559e9711e46bb18b12a7
kevin-zhao-career <71047577+kevin-zhao-career@users.noreply.github.com>
2025-03-20 11:44:41 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -18,3 +18,16 @@ sudo vim /etc/grub.d/09_windows
 sudo chmod o-w /etc/grub.d/09_windows
 sudo update-grub
 sudo cat /boot/grub/grub.cfg 
+
+sudo apt-get install mokutil openssl
+openssl req -new -x509 -newkey rsa:2048 -keyout MOK.priv -outform DER -out MOK.der -nodes -days 36500 -subj "/CN=Kevin Zhao/"
+sudo mokutil --import MOK.der
+sudo ubuntu-drivers list
+sudo ubuntu-drivers install
+sudo /usr/src/linux-headers-$(uname -r)/scripts/sign-file sha256 MOK.priv MOK.der $(modinfo -n nvidia-modeset)
+sudo /usr/src/linux-headers-$(uname -r)/scripts/sign-file sha256 MOK.priv MOK.der $(modinfo -n nvidia-uvm)
+sudo /usr/src/linux-headers-$(uname -r)/scripts/sign-file sha256 MOK.priv MOK.der $(modinfo -n nvidia)
+sudo /usr/src/linux-headers-$(uname -r)/scripts/sign-file sha256 MOK.priv MOK.der $(modinfo -n nvidia-drm)
+sudo /usr/src/linux-headers-$(uname -r)/scripts/sign-file sha256 MOK.priv MOK.der $(modinfo -n nvidia-peermem)
+sudo update-initramfs -u
+Step 8: Reboot Again and Verify
