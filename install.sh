4afedeb31e5b9eb3589fa2e51c345d2e27047439
kevin-zhao-career <71047577+kevin-zhao-career@users.noreply.github.com>
2025-03-19 23:23:15 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,3 +5,10 @@ sudo apt-get upgrade
 sudo mdadm --assemble --scan --verbose
 sudo apt install timeshift
 sudo add-apt-repository universe
+sudo apt-get install vim
+
+sudo fdisk -l
+sudo chmod o+w /etc/grub.d/09_windows
+sudo vim /etc/default/grub
+sudo chmod o-w /etc/grub.d/09_windows
+sudo update-grub
